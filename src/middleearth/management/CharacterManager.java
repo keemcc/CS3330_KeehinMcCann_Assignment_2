c6ee58dd298097d94d48be8a0822a5b2d66acf26
keemcc <ktsmccann@gmail.com>
2025-03-04 23:16:19 -0600

    Created CharacterManager class with method stubs

---
+++ src/middleearth/management/CharacterManager.java
@@ -0,0 +1,28 @@
+package middleearth.management;
+
+import middleearth.characters.MiddleEarthCharacter;
+
+public class CharacterManager {
+	private MiddleEarthCharacter[] characters;
+	private int size;
+	
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		return false;
+	}
+	
+	public MiddleEarthCharacter getCharacter(String name) {
+		return null;
+	}
+	
+	public boolean updateCharacter (MiddleEarthCharacter character, String name, int health, int power) {
+		return false;
+	}
+	
+	public boolean deleteCharactrer(MiddleEarthCharacter character) {
+		return false;
+	}
+	
+	public void displayAllCharacters() {
+		//Displays all characters
+	}
+}
