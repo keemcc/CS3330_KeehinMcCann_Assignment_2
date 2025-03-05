6561bb4fbf9db8cb10048951db31e2914afb35d0
keemcc <ktsmccann@gmail.com>
2025-03-04 21:32:32 -0600

    Created Wizard class and implemented getRace() method

---
+++ src/middleearth/characters/Wizard.java
@@ -0,0 +1,15 @@
+package middleearth.characters;
+
+public class Wizard extends MiddleEarthCharacter{
+
+	@Override
+	boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	String getRace() {
+		return "Wizard";
+	}
+}
