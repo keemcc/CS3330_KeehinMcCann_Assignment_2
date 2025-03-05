6c752403b9696e894644711ff89eb70145668562
keemcc <ktsmccann@gmail.com>
2025-03-04 21:27:06 -0600

    Created Elf class and implemented getRace() method

---
+++ src/middleearth/characters/Elf.java
@@ -0,0 +1,16 @@
+package middleearth.characters;
+
+public class Elf extends MiddleEarthCharacter{
+
+	@Override
+	boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	String getRace() {
+		return "Elf";
+	}
+
+}
