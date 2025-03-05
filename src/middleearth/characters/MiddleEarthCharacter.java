da0cc46a5614d8547f38eab52b5e710372bff8c3
keemcc <ktsmccann@gmail.com>
2025-03-04 21:11:04 -0600

    Added constructor, abstract methods, and displayInfo *Created MiddleEarthCharacter constructor which sets all fields to default values *Added abstract boolean attack(...) and abstract String getRace() methods *Added void displayInfo() which prints all character field values

--- src/middleearth/characters/MiddleEarthCharacter.java
+++ src/middleearth/characters/MiddleEarthCharacter.java
@@ -4,4 +4,19 @@ public abstract class MiddleEarthCharacter {
 	private String name;
 	private double health;
 	private double power;
+	
+	public MiddleEarthCharacter() {
+		name = "Default";
+		health = -1;
+		power = -1;
+	}
+	
+	abstract boolean attack(MiddleEarthCharacter target);
+	abstract String getRace();
+	
+	public void displayInfo() {
+		System.out.println("Character name = " + name);
+		System.out.println("Character health = " + health);
+		System.out.println("Character power = " + power);
+	}
 }
