dcb63e0be3e671e5603dc50c00ce40dd07a58246
keemcc <ktsmccann@gmail.com>
2025-03-04 21:28:19 -0600

    Created Dwarf class and implemented getRace() method

---
+++ src/middleearth/characters/Dwarf.java
@@ -0,0 +1,16 @@
+package middleearth.characters;
+
+public class Dwarf extends MiddleEarthCharacter {
+
+	@Override
+	boolean attack(MiddleEarthCharacter target) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	String getRace() {
+		return "Dwarf";
+	}
+
+}
