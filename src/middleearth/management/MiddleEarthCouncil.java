9be2833c928f76c3a034dfa5f81874e37d0da4ab
keemcc <ktsmccann@gmail.com>
2025-03-04 23:17:29 -0600

    Added CharacterManager and getCharacterManager() to MiddleEarthCouncil

--- src/middleearth/management/MiddleEarthCouncil.java
+++ src/middleearth/management/MiddleEarthCouncil.java
@@ -2,13 +2,19 @@ package middleearth.management;
 
 public class MiddleEarthCouncil {
 	private static MiddleEarthCouncil instance;
+	private CharacterManager charManager;
 	
 	private MiddleEarthCouncil() {}
 	
 	public static MiddleEarthCouncil getInstance() {
 		if (instance == null) {
 			instance = new MiddleEarthCouncil();
+			instance.charManager = new CharacterManager();
 		}
 		return instance;
+	}
+	
+	public CharacterManager getCharacterManager() {
+		return charManager;
 	}
 }
