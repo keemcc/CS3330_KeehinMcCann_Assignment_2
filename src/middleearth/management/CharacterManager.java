55e3de533db629bcef8b44c5ec4a854d6021628f
keemcc <ktsmccann@gmail.com>
2025-03-04 23:33:13 -0600

    Created addCharacter method

--- src/middleearth/management/CharacterManager.java
+++ src/middleearth/management/CharacterManager.java
@@ -7,12 +7,27 @@ public class CharacterManager {
 	private int size;
 	
 	public CharacterManager() {
-		characters = new MiddleEarthCharacter[0];
 		size = 0;
 	}
 	
 	public boolean addCharacter(MiddleEarthCharacter c) {
-		return false;
+		if (size == 0) {
+			size = 1;
+			characters = new MiddleEarthCharacter[1];
+			characters[0] = c;
+			return true;
+		}
+		
+		if (size == characters.length) {
+			MiddleEarthCharacter[] newCharacters = new MiddleEarthCharacter[(characters.length) * 2];
+			for (int i = 0; i < size; i++) {
+				newCharacters[i] = characters[i];
+			}
+			characters = newCharacters;
+		}
+		size++;
+		characters[size-1] = c;
+		return true;
 	}
 	
 	public MiddleEarthCharacter getCharacter(String name) {
