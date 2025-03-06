c369d22328493df433ea960a0961e411b28b61c3
keemcc <ktsmccann@gmail.com>
2025-03-05 19:50:48 -0600

    Added javadocs and comments to MiddleEarthApp

--- src/middleearth/application/MiddleEarthApp.java
+++ src/middleearth/application/MiddleEarthApp.java
@@ -2,13 +2,16 @@ package middleearth.application;
 
 import java.util.Scanner;
 
+/**
+ * Application class to run menu
+ */
 public class MiddleEarthApp {
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Menu menu = new Menu(scanner);
 		System.out.println("Welcome to Middle-Earth Madness!");
 		System.out.println();
+		//Repeat this section until the user wishes to exit
 		program:
 		while (true) {
 			switch (menu.displayMainMenu()) {
